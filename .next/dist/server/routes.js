9c5d9ee9bd91fff6033eaeef5161b2e351fb6f6c
Yichen <yichen_0820@yahoo.com>
2019-05-04 16:06:48 +0800

    first deplpoy

--- .next/dist/server/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/', '/index').add('/patient/signup', '/patient/signup');
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNlcnZlci9yb3V0ZXMuanMiXSwibmFtZXMiOlsicm91dGVzIiwicmVxdWlyZSIsImFkZCIsIm1vZHVsZSIsImV4cG9ydHMiXSwibWFwcGluZ3MiOiI7O0FBQUEsSUFBTSxTQUFTLEFBQWY7O0FBRUEsT0FDSyxBQURMLElBQ1MsQUFEVCxLQUNhLEFBRGIsVUFFSyxBQUZMLElBRVMsQUFGVCxtQkFFMkIsQUFGM0I7O0FBS0EsT0FBTyxBQUFQLFVBQWlCLEFBQWpCIiwiZmlsZSI6InJvdXRlcy5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvc2hhcnAvRGVza3RvcC9Tb2xpZGl0eS9Eb0NoYWluIn0=
